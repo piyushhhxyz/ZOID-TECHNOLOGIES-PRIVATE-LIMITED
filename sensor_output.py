c99e8627c8f636ee17a0a6ef16005a0fc2492b69
piyushhhxyz <piyush.bhawsar1@gmail.com>
2023-08-10 12:48:52 +0530

    Styling part - Task 1,2,3,4,5,6 DONE

--- sensor_output.py
+++ sensor_output.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 import random
 import time
 import threading
-from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -25,7 +24,7 @@ def data_generator():
                     "DetectionTime": random.randint(0, 23244)
                 }
             }
-        time.sleep(5)  # Change to 5 seconds
+        time.sleep(5)
 
 data_thread = threading.Thread(target=data_generator)
 data_thread.start()
@@ -39,7 +38,14 @@ def get_data():
     global sensor_data
     return jsonify(sensor_data)
 
-if __name__ == '__main__':
-    # app.run(host='localhost', port=5000)
-    app.run(host='localhost', port=8080)
+@app.route('/send-json', methods=['POST'])
+def receive_json():
+    try:
+        received_data = request.json
+        print("Received JSON:", received_data)
+        return jsonify({"message": "JSON received successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080)
